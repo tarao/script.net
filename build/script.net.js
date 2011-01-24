9512c83e06cdc228121b22e0d1795601ea42390c
tarao <tarao.gnn@gmail.com>
2011-01-24 05:30:41 +0000

    Modularize script runner.

--- build/script.net.js
+++ build/script.net.js
@@ -3,8 +3,6 @@
  */
 import System;
 import System.IO;
-import Microsoft.CSharp;
-import Microsoft.JScript;
 
 import GNN;
 
@@ -37,49 +35,10 @@ import GNN;
         return;
     }
 
-    var script = new GNN.Script();
-    if (named.target) script.options.target = named.target;
-
-    // import assemblies
-    if (named['import'] == 'none') {
-    } else if (named['import'] == 'standard') {
-        script.importStandardAssemblies();
-    } else {
-        script.importConfiguredAssemblies();
-    }
-
-    // FIXME: this should be added by a statement
-    // import GNN in 'GNN.Script.dll'
-    script.importAssemblies('GNN.Script.dll');
-
-    // select language
-    var provider=null; var m;
-    if (named.lang) {
-        if ('csharp'.indexOf(named.lang.toLowerCase()) == 0) {
-            provider = new CSharpCodeProvider();
-        } else if ('jscript'.indexOf(named.lang.toLowerCase()) == 0) {
-            provider = new JScriptCodeProvider();
-        }
-        if (!provider) {
-            Console.Error.WriteLine('Unknown language "'+named.lang+'"');
-            return;
-        }
-    } else if (/\.cs$/.test(fname)) {
-        provider = new CSharpCodeProvider();
-    } else {
-        provider = new JScriptCodeProvider();
-    }
-
-    // compile
-    var res = script.compile(fname, provider);
-    if (0 < res.Errors.Count) {
-        script.reportErrors(Console.Error);
-        return;
-    }
-
-    // run
-    var asm = res.CompiledAssembly;
-    var method = asm.EntryPoint;
-    var params : String[] = args.concat([]);
-    method.Invoke(null, [params]);
+    var runner = new GNN.ScriptRunner({
+        lang: named.lang,
+        target: named.target,
+        'import': named['import']
+    });
+    runner.run.apply(runner, [fname].concat(args));
 })();
