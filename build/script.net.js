82cc1262c86c85e274e3ee823fe87f72a4b6e2a6
tarao <tarao.gnn@gmail.com>
2011-02-19 11:14:28 +0000

    Options.

--- build/script.net.js
+++ build/script.net.js
@@ -33,10 +33,64 @@ import GNN;
             '<script>',
             'args...'
         ].join(' '));
+        Console.Out.WriteLine('Options:');
+        var options = [
+            [ '/run[+|-]',
+              'Immediately run after the compilation (default).' ],
+            [ '/debug[+|-]',
+              'Generate debug information.' ],
+            [ '/out:<file>',
+              'Output binary file.' ],
+            [ '/target:exe',
+              'Console application (default).' ],
+            [ '/target:winexe',
+              'Windows application.' ],
+            [ '/target:library',
+              'Library assembly.' ],
+            [ '/lang:jscript',
+              'Compile JScript source code (default).' ],
+            [ '/lang:csharp',
+              'Compile C# source code.' ],
+            [ '/import:none',
+              'Add no reference assemblies.' ],
+            [ '/import:standard',
+              'Add standard reference assemblies.' ],
+            [ '/import:configured',
+              'Add reference assemblies from .rsp file (default).' ],
+            [ '/autoref[+|-]',
+              'Automatically add references to imported assemblies.' ],
+            [ '',
+              '(JScript feature)' ],
+            [ '/fast[+|-]',
+              'Generate optimized code.' ],
+            [ '/optimize[+|-]',
+              'Generate optimized code.' ],
+            []
+        ]; options.pop();
+        var max=0; var i;
+        for (i=0; i < options.length; i++) {
+            if (max < options[i][0].length) max = options[i][0].length;
+        }
+        for (i=0; i < options.length; i++) {
+            var option = '  '+options[i][0];
+            var desc = options[i][1];
+            for (var j=max-option.length+4; 0 <= j ; j--) option += ' ';
+            Console.Out.WriteLine(option+desc);
+        }
         return;
     }
 
     var argv : String[] = args;
-    var r = GNN.Scripting.Runner.run(named, fname, argv);
-    Environment.ExitCode = r;
+    if (named.run == false) {
+        GNN.Scripting.Runner.using(function(runner) {
+            if (runner.load(fname, named)) {
+                Environment.ExitCode = 0;
+            } else {
+                Environment.ExitCode = 1;
+            }
+        });
+    } else {
+        var r = GNN.Scripting.Runner.run(named, fname, argv);
+        Environment.ExitCode = r;
+    }
 })();
