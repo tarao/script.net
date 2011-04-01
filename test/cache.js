b157222a639ab3e7d69c0959e8079bee44ab2021
tarao <tarao.gnn@gmail.com>
2011-04-01 00:17:33 +0000

    Use SetUp/TearDown and Datapoints/Theory.

--- test/cache.js
+++ test/cache.js
@@ -5,85 +5,102 @@ import GNN.Scripting;
 
 package GNN.Scripting.Test {
     TestFixture class CacheTest {
-        Test function CtorTest() {
-            Assert.That(new Cache({}), Is.Not.Null,
+        var length : int = 11;
+        var o : Object[] = new Object[this.length];
+        var c : Cache[] = new Cache[this.length];
+        var prefix : String[] = new String[this.length];
+        Datapoints var index : int[] = new int[this.length];
+
+        function CacheTest() {
+            for (var i : int = 0; i < this.index.length; i++) this.index[i]=i;
+        }
+
+        SetUp function Init() {
+            var data = [
+                [ {},
+                  'ee_'
+                ],
+                [ { input: 'foo' },
+                  'ee_'
+                ],
+                [ { input: 'bar' },
+                  'ee_'
+                ],
+                [ { input: 'foo', file: 'foo' },
+                  're_'
+                ],
+                [ { input: 'foo', target: 'winexe' },
+                  'ew_'
+                ],
+                [ { input: 'foo', target: 'library' },
+                  'el_'
+                ],
+                [ { input: 'foo', target: 'xxx' },
+                  'ex_'
+                ],
+                [ { input: 'foo', debug: true },
+                  'eed_'
+                ],
+                [ { input: 'bar', optimize: true },
+                  'eeo_'
+                ],
+                [ { input: 'foo', fast: true },
+                  'eeo_'
+                ],
+                [ { input: 'foo', file: 'foo',
+                    target: 'library', debug: true, fast: true
+                  },
+                  'rldo_'
+                ]
+            ];
+
+            for (var i : int = this.length-1; 0 <= i; i--) {
+                this.o[i] = data[i][0];
+                this.c[i] = new Cache(this.o[i]);
+                this.prefix[i] = data[i][1];
+            }
+        }
+
+        TearDown function Cleanup() {
+            for (var i : int = this.c.length-1; 0 <= i; i--) {
+                if (File.Exists(this.c[i].file)) File.Delete(this.c[i].file);
+                this.c[i] = null;
+            }
+        }
+
+        Theory function CtorTest(i : int) {
+            Assert.That(this.c[i], Is.Not.Null,
                         'Cache.ctor must return non-null value');
         }
 
-        Test function FileNameTest() {
-            var c1 : Cache = new Cache({});
-            var c2 : Cache = new Cache({ input: 'foo' });
-            var c3 : Cache = new Cache({ input: 'bar' });
+        Theory function FileNameTest(i : int) {
+            Assert.That(this.c[i].file, Is.Not.Null,
+                        'Cache file name must exist');
+            Assert.That(this.c[i].file, Is.Not.Empty,
+                        'Cache file name must exist');
 
-            Assert.That(c1.file, Is.Not.Null, 'Cache file name must exist');
-            Assert.That(c1.file, Is.Not.Empty, 'Cache file name must exist');
-            Assert.That(c2.file, Is.Not.Null, 'Cache file name must exist');
-            Assert.That(c2.file, Is.Not.Empty, 'Cache file name must exist');
-            Assert.That(c3.file, Is.Not.Null, 'Cache file name must exist');
-            Assert.That(c3.file, Is.Not.Empty, 'Cache file name must exist');
-        }
-
-        Test function DistinctFileNameTest() {
-            var c1 : Cache = new Cache({});
-            var c2 : Cache = new Cache({ input: 'foo' });
-            var c3 : Cache = new Cache({ input: 'bar' });
-
-            Assert.That(c1.file, Is.Not.EqualTo(c2.file),
+            Assert.That(this.c[i].file,
+                        Is.Not.EqualTo(this.c[(i+1) % this.c.length].file),
                         'Cache file names must be distinct');
-            Assert.That(c2.file, Is.Not.EqualTo(c3.file),
-                        'Cache file names must be distinct');
-            Assert.That(c3.file, Is.Not.EqualTo(c1.file),
-                        'Cache file names must be distinct');
-        }
 
-        Test function FileNameSpecTest() {
-            var c1 : Cache = new Cache({ input: 'foo' });
-            var c2 : Cache = new Cache({ input: 'foo', file: 'foo' });
-            var c3 : Cache = new Cache({ input: 'foo', target: 'winexe' });
-            var c4 : Cache = new Cache({ input: 'foo', target: 'library' });
-            var c5 : Cache = new Cache({ input: 'foo', target: 'xxx' });
-            var c6 : Cache = new Cache({ input: 'foo', debug: true });
-            var c7 : Cache = new Cache({ input: 'foo', optimize: true });
-            var c8 : Cache = new Cache({ input: 'foo', fast: true });
-            var c9 : Cache = new Cache({
-                input: 'foo', file: 'foo',
-                target: 'library', debug: true, fast: true
-            });
-
-            var msg = 'Cache file name must have prefix';
-            Assert.That(Path.GetFileName(c1.file), Is.StringStarting('ee_'),
-                        msg);
-            Assert.That(Path.GetFileName(c2.file), Is.StringStarting('re_'),
-                        msg);
-            Assert.That(Path.GetFileName(c3.file), Is.StringStarting('ew_'),
-                        msg);
-            Assert.That(Path.GetFileName(c4.file), Is.StringStarting('el_'),
-                        msg);
-            Assert.That(Path.GetFileName(c5.file), Is.StringStarting('ex_'),
-                        msg);
-            Assert.That(Path.GetFileName(c6.file), Is.StringStarting('eed_'),
-                        msg);
-            Assert.That(Path.GetFileName(c7.file), Is.StringStarting('eeo_'),
-                        msg);
-            Assert.That(Path.GetFileName(c8.file), Is.StringStarting('eeo_'),
-                        msg);
-            Assert.That(Path.GetFileName(c9.file), Is.StringStarting('rldo_'),
-                        msg);
+            Assert.That(Path.GetFileName(this.c[i].file),
+                        Is.StringStarting(this.prefix[i]),
+                        'Cache file name must have appropriate prefix');
         }
 
         Test function SourceUpdateTest() {
-            var c1 : Cache = new Cache({ input: 'foo' });
+            var c1 : Cache = this.c[1];
             var fname = c1.file;
 
             Assert.False(File.Exists(c1.file),
                          'Ctor must not write cache file');
-            Assert.True(c1.update('foo', {}),
+            Assert.True(c1.update(this.o[1].input, {}),
                         'The first update() always returns true');
             Assert.True(File.Exists(c1.file),
                         'Cache update must generate file');
 
-            var c2 : Cache = new Cache({ input: 'foo' });
-            Assert.False(c2.update('foo', {}),
+            var c2 : Cache = new Cache(this.o[1]);
+            Assert.False(c2.update(this.o[1].input, {}),
                          'The first update() with the same source returns ' +
                          'false if cache file already exist');
             Assert.That(c1.file, Is.EqualTo(c2.file),
@@ -99,14 +116,11 @@ package GNN.Scripting.Test {
             Assert.That(c1.file, Is.EqualTo(fname),
                         'Cache file name does not change during '+
                         'modification');
-
-            if (File.Exists(c1.file)) File.Delete(c1.file);
-            if (File.Exists(c2.file)) File.Delete(c2.file);
         }
 
         Test function OptionUpdateTest() {
             var s = 'hogehoge';
-            var o = { input: 'foo' };
+            var o = this.o[1];
             var c1 : Cache = new Cache(o);
             var fname = c1.file;
 
@@ -135,18 +149,13 @@ package GNN.Scripting.Test {
                          ' [hash]');
 
             Assert.That(c1.file, Is.EqualTo(fname),
-                        'Cache file name does not change during '+
+                        'Cache file name does not change during ' +
                         'modification');
 
             var c3 : Cache = new Cache(o);
             Assert.That(c1.file, Is.Not.EqualTo(c3.file),
-                        'Changing [file] option generates different '+
+                        'Changing [file] option generates different ' +
                         'file for new cache object');
-
-            if (File.Exists(fname)) File.Delete(fname);
-            if (File.Exists(c1.file)) File.Delete(c1.file);
-            if (File.Exists(c2.file)) File.Delete(c2.file);
-            if (File.Exists(c3.file)) File.Delete(c3.file);
         }
     }
 }
